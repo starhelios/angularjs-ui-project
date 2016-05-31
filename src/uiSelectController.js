angular-ui/ui-select

commit d78ba5f8f6e700143bdf3af1bfda0c05153daf45

feat(perf): optimize width resizing
- Optimize running dynamic width calculations when resizing

src/uiSelectController.js
@@ -444,6 +444,7 @@ uis.controller('uiSelectCtrl',
   };
 
   var sizeWatch = null;
+  var updaterScheduled = false;
   ctrl.sizeSearchInput = function() {
 
     var input = ctrl.searchInput[0],
@@ -465,10 +466,16 @@ uis.controller('uiSelectCtrl',
     ctrl.searchInput.css('width', '10px');
     $timeout(function() { //Give tags time to render correctly
       if (sizeWatch === null && !updateIfVisible(calculateContainerWidth())) {
-        sizeWatch = $scope.$watch(calculateContainerWidth, function(containerWidth) {
-          if (updateIfVisible(containerWidth)) {
-            sizeWatch();
-            sizeWatch = null;
+        sizeWatch = $scope.$watch(angular.noop, function() {
+          if (!updaterScheduled) {
+            updaterScheduled = true;
+            $scope.$$postDigest(function() {
+              updaterScheduled = false;
+              if (updateIfVisible(calculateContainerWidth())) {
+                sizeWatch();
+                sizeWatch = null;
+              }
+            });
           }
         });
       }

starhelios/angularjs-ui-project

commit 38be5df28586b07072b5cfd1141577aa04976779

fix: search input width resizing
Fixed an issue introduced by d78ba5f8f6e700143bdf3af1bfda0c05153daf45
The watcher was called only once since `angular.noop` will always return `undefined`.
Fixed this by calling the resize function on each $digest.

Closes #1618
Closes #1617
Closes #1575

src/uiSelectController.js
@@ -471,7 +471,7 @@ uis.controller('uiSelectCtrl',
     ctrl.searchInput.css('width', '10px');
     $timeout(function() { //Give tags time to render correctly
       if (sizeWatch === null && !updateIfVisible(calculateContainerWidth())) {
-        sizeWatch = $scope.$watch(angular.noop, function() {
+        sizeWatch = $scope.$watch(function() {
           if (!updaterScheduled) {
             updaterScheduled = true;
             $scope.$$postDigest(function() {
@@ -482,7 +482,7 @@ uis.controller('uiSelectCtrl',
               }
             });
           }
-        });
+        }, angular.noop);
       }
     });
   };